8bb9f2ccc61db71335f3e88db9191aa4ff13884b
dariomicelii <dariomiceli01@gmail.com>
2024-12-11 18:47:37 +0100

    posts table

--- src/Pages/PostsPage.jsx
+++ src/Pages/PostsPage.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Posts() {
   const [posts, setPosts] = useState([]);
+  const localhost = "http://localhost:3000/posts";
 
   const fetchPosts = () => {
-    fetch("http://localhost:3000/posts")
+    fetch(localhost)
       .then((res) => res.json())
       .then((data) => {
         setPosts(data);
@@ -13,13 +15,44 @@ export default function Posts() {
   useEffect(() => {
     fetchPosts();
   }, []);
+
   return (
     <div className="container my-5">
-      <ul>
-        {posts.map((post, id) => (
-          <li key={id}>{post.title}</li>
-        ))}
-      </ul>
+      <div>
+        <h1>Rolling Stones Archives</h1>
+      </div>
+      <table className="table table-dark my-5">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Preview</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Categoria</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post) => (
+            <tr key={post.id}>
+              <th scope="row">{post.id}</th>
+              <td>
+                <img src={post.image} width="60" />
+              </td>
+              <td>{post.title}</td>
+              <td>{post.category}</td>
+              <td>
+                <Link
+                  to={`/posts/${post.id}`}
+                  type="button"
+                  className="btn btn-primary"
+                >
+                  Dettaglio
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
